d143b0277aa7b51938d785db8a40560868d2af46
MargheritaPhilipp <91319540+MargheritaPhilipp@users.noreply.github.com>
2023-05-03 17:07:59 +0200

    :round_pushpin: EDA on locations
    Explored a bit more on adm_1 and adm_2. Want to check out the polygons next.

--- uppsala_data.py
+++ uppsala_data.py
@@ -45,17 +45,273 @@ print(df1.date_start.min())
 print(df1.date_start.max())
 df1.head()
 
-#%%
-## SKIP THIS AS TAKES LONGER
-# Import and inspect UCDP GED data set (1989-2021) via API
 
-api_url = 'https://ucdpapi.pcr.uu.se/api/gedevents/22.1' 
+# %%
+#####################################################################################################################
+# Data set 2
+#####################################################################################################################
+
+# Import and inspect UCDP Candidate data sets (2022-2023) via API
+# January to December 2022: Version 22.01.22.12
+# January 2023: Version 23.0.1
+# February 2023: Version 23.0.2 
+# March 2023: Version 23.0.3 
+
+
+link_prefix = 'https://ucdpapi.pcr.uu.se/api/gedevents/'
+# not yet available (on 3 May 23) but should be out soon: '23.0.4'
+link_suffixes = ['23.0.3', '23.0.2', '23.0.1', '22.01.22.12']
+
+api_urls = []
+
+for suffix in link_suffixes:
+    full_link = link_prefix + str(suffix)
+    api_urls.append(full_link)
+
+print(api_urls)
+
 params = {
     'pagesize': 1000,
     'page': 0
 }
-response = requests.get(api_url, params=params)
-print(response.status_code)
+#response = requests.get(api_url2, params=params)
+#print(response.status_code)
+
+#%%
+
+# Initialise empty dataframe
+df_2 = pd.DataFrame()
+
+for url in api_urls:
+    response = requests.get(url, params=params)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        json_data = response.json()
+        
+        # Get the total number of pages
+        total_pages = json_data['TotalPages']
+        
+        # Extract the 'Result' array from the JSON response
+        data = json_data['Result']
+
+        # Convert JSON data to a pandas dataframe
+        df2 = pd.DataFrame(data)
+
+        # Iterate through the rest of the pages and append data to the DataFrame
+        for page in range(1, total_pages + 1):
+            params['page'] = page
+            response = requests.get(url, params=params)
+            json_data = response.json()
+            data = json_data['Result']
+            temp_df = pd.DataFrame(data)
+            df2 = pd.concat([df2, temp_df], ignore_index=True)
+            
+            # Print progress
+            print(f"Page {page} of {total_pages} fetched")
+
+        # Save the DataFrame to a CSV file
+        #df2.to_csv('ucdp_api_to_dataframe.csv', index=False)
+
+        # Display the dataframe
+        print(df2)
+        # Attache df2 for this url to overall df_2
+        df_2 = pd.concat([df2,df_2], ignore_index=True)
+    else:
+        print(f"Error: {response.status_code}. {response.text}")
+
+
+#%%
+print(df_2.shape)
+#print(df_new.shape)
+
+#print(df2.columns)
+print(df_2.date_start.min())
+print(df_2.date_start.max())
+#df.iloc[3000]
+df_2.head()
+
+
+# %%
+#####################################################################################################################
+# Data sets combined 
+#####################################################################################################################
+
+#check they have the same column headings
+print(set(df1.columns) - set(df_2.columns))
+
+#%%
+# Combine UCDP GED data set (1989-2021) and UCDP Candidate data sets (2022-2023)
+df_all = pd.concat([df1, df_2], ignore_index=True)
+
+#%%
+print(df_all.shape)
+#print(df_all.columns)
+print(df_all.date_start.min())
+print(df_all.date_start.max())
+
+# %%
+
+#####################################################################################################################
+# EDA
+#####################################################################################################################
+
+# Overview of missingness
+df_all.isna().sum().T
+
+#%%
+# Overview of unique values
+df_all.agg(['size', 'count', 'nunique']).T #difference between size and count are the missing values
+
+# %%
+# conflicts per year
+df_all.year.value_counts().sort_index(axis=0).plot.bar()
+plt.show()
+
+#%%
+
+#conflict regions
+df_all.region.value_counts().plot.bar()
+plt.show()
+
+#country
+plt.rcParams["figure.figsize"] = (20,6)
+df_all.country.value_counts().plot.bar()
+plt.show()
+
+#type_of_violence Type of UCDP conflict:
+#1: state-based conflict
+#2: non-state conflict
+#3: one-sided violence
+plt.rcParams["figure.figsize"] = (8,6)
+df_all.type_of_violence.value_counts().plot.bar()
+plt.show()
+
+
+#active_year:
+#1: if the event belongs to an active conflict/dyad/actoryear
+#0: otherwise 
+df_all.active_year.value_counts().plot.bar()
+plt.show()
+
+# Most entries are "clear" - this code_status variable is not even inlcuded in the codebook for the GED data set
+# "Clear is used when we know what parties are involved, that the actors are organized enough (see definition of organized actor),
+# that there is a reported number of deaths,that we trust the source, that we have geo-coded the location, and for state-based
+# violence that we have identified a stated incompatibility. The clear events are included
+# in UCDP GED if the dyad has ever reached the 25-fatality threshold for inclusion."
+df_all.code_status.value_counts().plot.bar()
+plt.show()
+
+
+#%%
+
+#event_clarity
+df_all.event_clarity.value_counts().plot.bar()
+plt.show()
+
+#date_prec
+df_all.date_prec.value_counts().plot.bar()
+plt.show()
+
+#number_of_sources
+df_all.number_of_sources.value_counts().plot.bar()
+
+
+# %%
+
+# where_prec: "The precision with which the coordinates and location assigned to the event reflects the location of the actual event."
+# from "1: exact location of the event known and coded." to
+# "6: only the country where the event took place in is known." and "7: event in international waters or airspace."
+df_all.where_prec.value_counts().plot.bar()
+plt.show()
+
+# location description
+df_all['where_description']	
+
+
+#%%
+# Inspecting administration levels available per country
+
+# There are a lot of missing values for the admin layers
+print('Missing values')
+print(df_all[['adm_1', 'adm_2', 'country']].isna().sum())
+print(grouped_admin_overview[['adm_1', 'adm_2', 'country']].isna().sum())
+
+# We group them without dropping nan values
+grouped_admin_overview = df_all.groupby('country')[['adm_1', 'adm_2']].nunique(dropna = False).reset_index() #(name='count')
+
+print(grouped_admin_overview.head())
+grouped_admin_overview.describe().applymap(lambda x: f"{x:0.2f}")
+
+
+#%%
+# There are 127 countries in both the overall and the grouped data frame
+countries_all = df_all['country'].unique()
+countries_grouped = grouped_admin_overview['country'].unique()
+
+print('Number of countries in df:' + str(len(countries_all)))
+print('Number of countries in groupby: ' + str(len(countries_grouped)))
+
+# this issue has now been resolved
+# for some reason the grouped table does not include {'Belarus', 'Dominican Republic', 'Poland', 'Sao Tome and Principe'}
+print('Countries missing from groupby')
+print(set(countries_all) - set(countries_grouped))
+
+# although inspecting further, only Belarus's event has no adm data
+#no_data_counrties = ['Belarus', 'Dominican Republic', 'Poland', 'Sao Tome and Principe']
+
+#for i in range(len(no_data_counrties)):
+#    print(df_all[df_all['country']==no_data_counrties[i]][['country','adm_1', 'adm_2']])
+
+
+#%%
+# Inspecting adm1 (e.g. province) and adm2 (e.g. municipality)
+grouped_admin1_detail = df_all.groupby(['country', 'adm_1']).size().reset_index(name='count')
+grouped_admin2_detail = df_all.groupby(['country', 'adm_2']).size().reset_index(name='count')
+
+print(grouped_admin1_detail.country.nunique())
+print(grouped_admin1_detail.country.nunique(dropna=False))
+
+print(grouped_admin2_detail.country.nunique())
+print(grouped_admin2_detail.country.nunique(dropna=False))
+
+# exmples for Angola: 18 adm1 and 149 adm2
+print(grouped_admin1_detail[grouped_admin1_detail['country']=='Angola'])
+print(grouped_admin2_detail[grouped_admin2_detail['country']=='Angola'])
+
+#%%
+# exmples for Germany: 18 adm1 and 149 adm2
+print(grouped_admin1_detail[grouped_admin1_detail['country']=='Germany'])
+print(grouped_admin2_detail[grouped_admin2_detail['country']=='Germany'])
+
+df_all[df_all['country']=='Germany'][['id', 'relid', 'year', 'active_year','side_a','side_b' ,'adm_1','adm_2', 'date_start', 'date_end', 'deaths_a',
+       'deaths_b', 'deaths_civilians', 'deaths_unknown',]]
+
+#%%
+fig, ax = plt.subplots(figsize=(10, 5))
+grouped_admin_overview.plot(kind='bar', x='country', y=['adm_1', 'adm_2'], ax=ax)
+ax.set_title('Count of unique adm_1 and adm_2 per country')
+ax.set_xlabel('Country')
+ax.set_ylabel('Count')
+plt.show()
+
+
+
+# %%
+#%%
+##################################
+# OLD WAY OF GETTING DF1
+##################################
+## SKIP THIS AS TAKES LONGER
+# Import and inspect UCDP GED data set (1989-2021) via API
+
+#api_url = 'https://ucdpapi.pcr.uu.se/api/gedevents/22.1' 
+#params = {
+#    'pagesize': 1000,
+#    'page': 0
+#}
+#response = requests.get(api_url, params=params)
+#print(response.status_code)
 
 #%%%
 
@@ -99,174 +355,3 @@ print(df.date_start.min())
 print(df.date_start.max())
 #df.iloc[3000]
 df.head()
-
-
-# %%
-#####################################################################################################################
-# Data set 2
-#####################################################################################################################
-
-# Import and inspect UCDP Candidate data sets (2022-2023) via API
-# January to December 2022: Version 22.01.22.12
-# January 2023: Version 23.0.1
-# February 2023: Version 23.0.2 
-# March 2023: Version 23.0.3 
-
-
-link_prefix = 'https://ucdpapi.pcr.uu.se/api/gedevents/'
-link_suffixes = ['23.0.3', '23.0.2', '23.0.1', '22.01.22.12']
-
-api_urls = []
-
-for suffix in link_suffixes:
-    full_link = link_prefix + str(suffix)
-    api_urls.append(full_link)
-
-print(api_urls)
-
-params = {
-    'pagesize': 1000,
-    'page': 0
-}
-#response = requests.get(api_url2, params=params)
-#print(response.status_code)
-
-#%%
-for url in api_urls:
-    response = requests.get(url, params=params)
-
-    # Check if request was successful
-    if response.status_code == 200:
-        json_data = response.json()
-        
-        # Get the total number of pages
-        total_pages = json_data['TotalPages']
-        
-        # Extract the 'Result' array from the JSON response
-        data = json_data['Result']
-
-        # Convert JSON data to a pandas dataframe
-        df2 = pd.DataFrame(data)
-
-        # Iterate through the rest of the pages and append data to the DataFrame
-        for page in range(1, total_pages + 1):
-            params['page'] = page
-            response = requests.get(url, params=params)
-            json_data = response.json()
-            data = json_data['Result']
-            temp_df = pd.DataFrame(data)
-            df2 = pd.concat([df2, temp_df], ignore_index=True)
-            
-            # Print progress
-            print(f"Page {page} of {total_pages} fetched")
-
-        # Save the DataFrame to a CSV file
-        df2.to_csv('ucdp_api_to_dataframe.csv', index=False)
-
-        # Display the dataframe
-        print(df2)
-    else:
-        print(f"Error: {response.status_code}. {response.text}")
-
-#%%
-print(df2.shape)
-#print(df_new.shape)
-
-#print(df2.columns)
-print(df2.date_start.min())
-print(df2.date_start.max())
-#df.iloc[3000]
-df2.head()
-
-
-# %%
-
-#####################################################################################################################
-# Data sets combined 
-#####################################################################################################################
-
-#check they have the same column headings
-print(set(df1.columns) - set(df2.columns))
-
-#%%
-# Combine UCDP GED data set (1989-2021) and UCDP Candidate data sets (2022-2023)
-df_all = pd.concat([df1, df2], ignore_index=True)
-
-#%%
-print(df_all.shape)
-#print(df_all.columns)
-print(df_all.date_start.min())
-print(df_all.date_start.max())
-
-# %%
-
-#####################################################################################################################
-# EDA
-#####################################################################################################################
-
-# Overview of missingness
-print(df_all.isna().sum())
-
-# Overview of unique values
-df_all.agg(['size', 'count', 'nunique']).T #difference between size and count are the missing values
-
-# %%
-# conflicts per year
-df_all.year.value_counts().sort_index(axis=0).plot.bar()
-plt.show()
-
-#%%
-
-#conflict regions
-df_all.region.value_counts().plot.bar()
-plt.show()
-
-#country
-plt.rcParams["figure.figsize"] = (20,6)
-df_all.country.value_counts().plot.bar()
-plt.show()
-
-#type_of_violence Type of UCDP conflict:
-#1: state-based conflict
-#2: non-state conflict
-#3: one-sided violence
-plt.rcParams["figure.figsize"] = (8,6)
-df_all.type_of_violence.value_counts().plot.bar()
-plt.show()
-
-# where_prec: "The precision with which the coordinates and location assigned to the event reflects the location of the actual event."
-# from "1: exact location of the event known and coded." to
-# "6: only the country where the event took place in is known." and "7: event in international waters or airspace."
-df_all.where_prec.value_counts().plot.bar()
-plt.show()
-
-#active_year:
-#1: if the event belongs to an active conflict/dyad/actoryear
-#0: otherwise 
-df_all.active_year.value_counts().plot.bar()
-plt.show()
-
-# Most entries are "clear" - this code_status variable is not even inlcuded in the codebook for the GED data set
-# "Clear is used when we know what parties are involved, that the actors are organized enough (see definition of organized actor),
-# that there is a reported number of deaths,that we trust the source, that we have geo-coded the location, and for state-based
-# violence that we have identified a stated incompatibility. The clear events are included
-# in UCDP GED if the dyad has ever reached the 25-fatality threshold for inclusion."
-df_all.code_status.value_counts().plot.bar()
-plt.show()
-
-
-#%%
-
-#event_clarity
-df_all.event_clarity.value_counts().plot.bar()
-plt.show()
-
-#date_prec
-df_all.date_prec.value_counts().plot.bar()
-plt.show()
-
-
-# %%
-
-
-# %%
